0a2c56601001ec12e4d0c78f2cce1be21f94ef2a
Shivansh B Aggarwal <bhupeshamazon2072@gmail.com>
2023-10-03 23:47:10 +0530

    image_input_complete

--- src/components/ImageInputs.js
+++ src/components/ImageInputs.js
@@ -1,7 +1,10 @@
 
 import React, { useState } from 'react';
+import {useForm} from 'react-hook-form'
+
 
 const ImageInputs = () => {
+
     const [selectedFile, setSelectedFile] = useState(null);
 
     const handleFileChange = (event) => {
@@ -20,22 +23,71 @@ const ImageInputs = () => {
             setSelectedFile(null);
         }
     };
+    const handleDelete =(event) =>{
+        
+        if(selectedFile){
+            setSelectedFile(null)
+        }
+    }
+    function openCamera() {
+        
+        const cameraStream = document.getElementById('cameraStream');
+        
+        // Check if the getUserMedia API is available in the user's browser
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices
+            .getUserMedia({ video: true }) // Request access to the camera
+            .then((stream) => {
+              // Display the camera stream in a video element
+              cameraStream.style.display = 'block';
+              cameraStream.srcObject = stream;
+            })
+            .catch((error) => {
+              console.error('Error accessing the camera:', error);
+            });
+        } else {
+          console.error('getUserMedia is not supported in this browser');
+        }
+      }  
 
     return (
-        <div className="offset-3 col-6 photocontainer p-0">
-        <div className="col-12 bg-light">
-            <div className="text-start p-3" htmlFor="fileName" onClick={() => document.getElementById('fileName').click()}>
-                Choose File
-                <input type="file" name="" id="fileName" style={{ display: 'none' }} onChange={handleFileChange} accept=".jpg, .jpeg, .png"/>
-            </div>
-            {selectedFile && (
-                <div className="preview-container text-start p-0 pb-3 m-0">
-                    <div className="container"><hr/></div>
-                    <small className='mx-2 mt-0'><span>File Name: {selectedFile.name}</span></small>
-                    <small className='mx-2'><span>File Size: {(selectedFile.size/(1024)).toFixed(2)} KB</span> ✅ <span onMouseEnter={(e)=>e.target.style.cursor="pointer"} oncli>❌</span></small>
+        <div className="offset-3 col-6 photocontainer p-0" onMouseEnter={(e)=>e.target.style.outline="none"}>
+            <div className="col-12 bg-light" >
+                <div 
+                    className="text-start p-3 d-flex justify-content-between fs-4 align-items-center fw-lighter" 
+                    htmlFor="fileName" 
+                    onClick={() => document.getElementById('fileName').click()} 
+                    onMouseEnter={(e)=>e.target.style.outline="1px dashed grey"} 
+                    onMouseLeave={(e)=>e.target.style.outline="none"}>
+                        Choose File
+                    <div >
+                        <span 
+                            className="material-symbols-outlined m-2 bg-secondary-subtle p-2 rounded-circle outline-none" 
+                            onMouseEnter={(e)=>e.target.style.cursor="pointer"}
+                        >
+                            upload
+                        </span>
+                        <span 
+                            className="material-symbols-outlined m-2 bg-secondary-subtle p-2 rounded-circle outline-none" 
+                            onMouseEnter={(e)=>e.target.style.cursor="pointer"} 
+                            onClick={openCamera}
+                        >
+                            photo_camera 
+                        </span>
+                    </div>
+                    <input 
+                        type="file" name="" id="fileName" 
+                        style={{ display: 'none' }} onChange={handleFileChange} 
+                        accept=".jpg, .jpeg, .png"
+                    />
                 </div>
-            )}
-        </div>
+                {selectedFile && (
+                    <div className="preview-container text-start p-0 m-0 fw-lighter">
+                        <span className='mx-2 mt-0 outline-none'>File Name: {selectedFile.name}</span>
+                        <span className='mx-2 outline-none'>File Size: {(selectedFile.size/(1024)).toFixed(2)} KB</span> ✅ <span onMouseEnter={(e)=>e.target.style.cursor="pointer"} onClick={handleDelete}>❌</span>
+                    </div>
+                )}
+            </div>
             <p className='my-0 text-start fw-lighter ' style={{fontSize: "15px"}}><i>Passport Size Photo in JPG format - JPG/PNG format less than 5 MB</i></p> 
         </div>  
     );
@@ -46,22 +98,4 @@ export default ImageInputs;
 
 
 
-{/* function openCamera() {
-  const cameraStream = document.getElementById('cameraStream');
-  
-  // Check if the getUserMedia API is available in the user's browser
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices
-      .getUserMedia({ video: true }) // Request access to the camera
-      .then((stream) => {
-        // Display the camera stream in a video element
-        cameraStream.style.display = 'block';
-        cameraStream.srcObject = stream;
-      })
-      .catch((error) => {
-        console.error('Error accessing the camera:', error);
-      });
-  } else {
-    console.error('getUserMedia is not supported in this browser');
-  }
-}  */}
+
--- src/pages/AdmissionForm.js
+++ src/pages/AdmissionForm.js
@@ -11,27 +11,44 @@ function AdmissionForm() {
                     <div className="row">
                         <div className="card-title text-start col-3 text-primary "><h3>Personal Details</h3></div><hr />
                     </div>
+
                     <div className="row photo">
                         <label htmlFor="photo" className="text-start col-3"><b>Your Photo</b></label>
-                        <ImageInputs/>     
-                                     
+                        <ImageInputs />
+
                     </div>
                     <div className="row fullname">
-                        <label htmlFor="photo" className="text-start col-3"><b>Full Name <span className='text-danger'>*</span></b></label>
-                        <div className="offset-3 col-6 bg-danger p-3">
-                            <div className="row">
-                                <div class="col-md-3 ">
-                                    <select id="inputState" className="form-select ">
-                                        <option selected >-Select-</option>
-                                        <option>...</option>
+                        <label htmlFor="fullName" className="text-start col-3"><b>Full Name <span className='text-danger'>*</span></b></label>
+                        <div className="offset-3 col-6">
+                            <div className="row gx-3">
+                                <div class="col-sm-2 p-0 ">
+                                    <select id="inputState" className="form-select " aria-label=''>
+                                        <option selected>-Select-</option>
+                                        <option>Mr.</option>
+                                        <option>Ms.</option>
+                                        <option>Mrs.</option>
                                     </select>
                                 </div>
-                                <input type='text' className="col"></input>
-                                <input type='text' className="col"></input>
-                                <input type='text' className="col"></input>
+                                <div class="col-sm-3 ">
+                                    <input type="text" class="form-control" id="specificSizeInputName" />
+                                    <label for="specificSizeInputName">First</label>
+                                </div>
+                                <div class="col-sm-3 ">
+                                    <input type="text" class="form-control" id="specificSizeInputName" />
+                                    <label for="specificSizeInputName">Middle</label>
+                                </div>
+                                <div class="col-sm-4">
+                                    <input type="text" class="form-control" id="specificSizeInputName" />
+                                    <label for="specificSizeInputName">Last</label>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className="row mail">
+                        <label htmlFor="photo" className="text-start col-3"><b>Email</b></label>
+
+                    </div>
+
                 </div>
 
             </div>
