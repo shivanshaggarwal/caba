46e5755c6be9c3656bf9a3c1767342d58e89a147
shivanshaggarwal <bhupeshamazon2072@gmail.com>
2023-10-18 10:38:54 +0530

    UI changes

--- src/components/FeeDetails/index.js
+++ src/components/FeeDetails/index.js
@@ -69,47 +69,49 @@ const FeeDetails = (props) => {
                 <div className="card-title text-start col-3 text-danger " >
                     <h4>Fee Details</h4>
                 </div>
-                <hr style={{border: "2px dashed"}}/>
-                <div className="row m-0 mb-4">
-                    <label htmlFor="" className="text-start col-3"><b>Total Course Fee</b></label>
-                    <div className="offset-2 col-4 p-0">
-                        <div className='d-flex align-items-center'>
-                            <input disabled type="text" className={`form-control ${formik.errors.courseFee && formik.touched.courseFee ? "border-danger" : ""}`} id="courseFee" onBlur={formik.handleBlur('courseFee')} value={formik.values.courseFee} onChange={(e) => formik.setFieldValue('courseFee', e.target.value)} />
-                            <h5 className='ms-3'>INR</h5>
+                <hr style={{border: "2px dashed", margin: "0px"}}/>
+                <div className="bg-secondary-subtle m-0 p-2">
+                    <div className="row m-0 mb-4">
+                        <label htmlFor="" className="text-start col-3"><b>Total Course Fee</b></label>
+                        <div className="offset-2 col-4 p-0">
+                            <div className='d-flex align-items-center'>
+                                <input disabled type="text" className={`form-control ${formik.errors.courseFee && formik.touched.courseFee ? "border-danger" : ""}`} id="courseFee" onBlur={formik.handleBlur('courseFee')} value={formik.values.courseFee} onChange={(e) => formik.setFieldValue('courseFee', e.target.value)} />
+                                <h5 className='ms-3'>INR</h5>
+                            </div>
+                            {formik.errors.courseFee && formik.touched.courseFee ? (
+                                <div className='text-danger text-start'>{formik.errors.courseFee}</div>
+                            ) : null}
                         </div>
-                        {formik.errors.courseFee && formik.touched.courseFee ? (
-                            <div className='text-danger text-start'>{formik.errors.courseFee}</div>
-                        ) : null}
                     </div>
-                </div>
-                <div className="row m-0 mb-4">
-                    <label htmlFor="" className="text-start col-3"><b>Admission Fee</b></label>
-                    <div className="offset-2 col-7 p-0">
-                        <input disabled type="text" className={`form-control ${formik.errors.admissionFee && formik.touched.admissionFee ? "border-danger" : ""}`} id="admissionFee" onBlur={formik.handleBlur('admissionFee')} value={formik.values.admissionFee} onChange={(e) => formik.setFieldValue('admissionFee', e.target.value)} />
-                        {formik.errors.admissionFee && formik.touched.admissionFee ? (
-                            <div className='text-danger text-start'>{formik.errors.admissionFee}</div>
-                        ) : null}
+                    <div className="row m-0 mb-4">
+                        <label htmlFor="" className="text-start col-3"><b>Admission Fee</b></label>
+                        <div className="offset-2 col-7 p-0">
+                            <input disabled type="text" className={`form-control ${formik.errors.admissionFee && formik.touched.admissionFee ? "border-danger" : ""}`} id="admissionFee" onBlur={formik.handleBlur('admissionFee')} value={formik.values.admissionFee} onChange={(e) => formik.setFieldValue('admissionFee', e.target.value)} />
+                            {formik.errors.admissionFee && formik.touched.admissionFee ? (
+                                <div className='text-danger text-start'>{formik.errors.admissionFee}</div>
+                            ) : null}
+                        </div>
                     </div>
-                </div>
-                <div className="row m-0 mb-4">
-                    <label htmlFor="photo" className="text-start col-3"><b>Montly Installment</b></label>
-                    <div className="offset-2 col-7 p-0">
-                        <input disabled type="text" className={`form-control ${formik.errors.monthlyInstallment && formik.touched.monthlyInstallment ? "border-danger" : ""}`} id="monthlyInstallment" onBlur={formik.handleBlur('monthlyInstallment')} value={formik.values.monthlyInstallment} onChange={(e) => formik.setFieldValue('monthlyInstallment', e.target.value)} />
-                        {formik.errors.monthlyInstallment && formik.touched.monthlyInstallment ? (
-                            <div className='text-danger text-start'>{formik.errors.monthlyInstallment}</div>
-                        ) : null}
+                    <div className="row m-0 mb-4">
+                        <label htmlFor="photo" className="text-start col-3"><b>Montly Installment</b></label>
+                        <div className="offset-2 col-7 p-0">
+                            <input disabled type="text" className={`form-control ${formik.errors.monthlyInstallment && formik.touched.monthlyInstallment ? "border-danger" : ""}`} id="monthlyInstallment" onBlur={formik.handleBlur('monthlyInstallment')} value={formik.values.monthlyInstallment} onChange={(e) => formik.setFieldValue('monthlyInstallment', e.target.value)} />
+                            {formik.errors.monthlyInstallment && formik.touched.monthlyInstallment ? (
+                                <div className='text-danger text-start'>{formik.errors.monthlyInstallment}</div>
+                            ) : null}
+                        </div>
                     </div>
-                </div>
-                <div className="row trainingType m-0 mb-4">
-                    <label htmlFor="photo" className="text-start col-3"><b>Fee Mode</b></label>
-                    <div className="offset-2 col-4 p-0">
-                        <select disabled id="inputState" className="form-select p-1" value={formik.values.feeMode} onChange={(e) => formik.setFieldValue('feeMode', e.target.value)} aria-label=''>
-                            <option selected className='fs-6 fw-lighter'>Installments or One Time</option>
-                            <option>Installments</option>
-                            <option>Online</option>
-                            <option>2 Part</option>
-                        </select>
-                        <p className='my-0 text-start fw-lighter fs-6 fst-italic'>Installment Plan - 15000 Admission Fee 6000 Montly from Next Month</p>
+                    <div className="row trainingType m-0 mb-4">
+                        <label htmlFor="photo" className="text-start col-3"><b>Fee Mode</b></label>
+                        <div className="offset-2 col-4 p-0">
+                            <select disabled id="inputState" className="form-select p-1" value={formik.values.feeMode} onChange={(e) => formik.setFieldValue('feeMode', e.target.value)} aria-label=''>
+                                <option selected className='fs-6 fw-lighter'>Installments or One Time</option>
+                                <option>Installments</option>
+                                <option>Online</option>
+                                <option>2 Part</option>
+                            </select>
+                            <p className='my-0 text-start fw-lighter fs-6 fst-italic'>Installment Plan - 15000 Admission Fee 6000 Montly from Next Month</p>
+                        </div>
                     </div>
                 </div>
                 <hr style={{border: "2px dashed"}}/>
@@ -142,6 +144,7 @@ const FeeDetails = (props) => {
                         </div>
                     </div>
                 </div>
+                
             </div>
         </>
     )
